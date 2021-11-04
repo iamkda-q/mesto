04c797cebc139871ab9a54fd47939edf27692716
KDA <iamkda@mail.ru>
2021-11-04 20:45:54 +0300

    add: add script for empty info and title_attribute for figcaption and info

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,7 @@ const profileName = document.querySelector('.profile__name');
 const profileVocation = document.querySelector('.profile__vocation');
 const formName = document.querySelector('.popup__text_parameter_name');
 const formVocation = document.querySelector('.popup__text_parameter_vocation');
+let listOfCaption = document.querySelectorAll('.gallery__figcaption');
 /* Обработчик изменения данных профиля */
 let editForm = document.querySelector('.popup__container'); //находим форму
 
@@ -22,10 +23,22 @@ closeEditPopup = () => popup.classList.remove('popup_opened');
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); // отменяет стандартную отправку формы.
-    profileName.textContent = formName.value;
-    profileVocation.textContent = formVocation.value;
+    profileName.textContent = formName.value.trim() ? formName.value : 'Без имени'; /* .trim() - удаляет пробелы с начала и конца строки, и повторяющиеся пробелы*/
+    profileVocation.textContent = formVocation.value.trim() ? formVocation.value : 'Без хобби';
+    profileName.setAttribute('title', profileName.textContent);
+    profileVocation.setAttribute('title', profileVocation.textContent);
     closeEditPopup();
 }
+
+/* Автоматическое добавление title к фотографиям */
+function addTitle (list) {
+  for (let item of list) {
+    item.setAttribute('title', item.textContent);
+  }
+}
+
+addTitle(listOfCaption);
+
 /* Открытие/закртыие popup */
 editProfileButton.addEventListener('click', showEditPopup);
 popupCloseButton.addEventListener('click', closeEditPopup);
