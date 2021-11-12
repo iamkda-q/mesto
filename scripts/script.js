873a26e033144d49ac0940d6f8fb4b002f8bafd8
KDA <iamkda@mail.ru>
2021-11-12 20:08:03 +0300

    fix: improve codestyle

--- scripts/script.js
+++ scripts/script.js
@@ -1,106 +1,38 @@
-'use srrict'
-
-/* Объявление переменных */
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-/* Попап изменения данных о себе любимом */
-const editProfileButton = document.querySelector('.profile__edit-profile-button');
-const popupEditProfile = document.querySelector('#edit-profile');
-const popupEditProfileCloseButton = popupEditProfile.querySelector('.popup__close-button');
-const formName = popupEditProfile.querySelector('.popup__text_parameter_name');
-const formVocation = popupEditProfile.querySelector('.popup__text_parameter_vocation');
-const popupEditProfileForm = popupEditProfile.querySelector('.popup__container');
-
-/* Попап добавления фотографии в галерею */
-const addPhotoButton = document.querySelector('.profile__edit-gallery-button');
-const popupAddPhoto = document.querySelector('#add-photo');
-const popupAddPhotoCloseButton = popupAddPhoto.querySelector('.popup__close-button');
-const formFigcaption = popupAddPhoto.querySelector('.popup__text_parameter_figcaption');
-const formPhotoLink = popupAddPhoto.querySelector('.popup__text_parameter_photo-link');
-const popupAddPhotoForm = popupAddPhoto.querySelector('.popup__container');
-
-/* Попап открытия фотографий */
-const popupFullPhoto = document.querySelector('#full-photo');
-const popupFullPhotoCloseButton = popupFullPhoto.querySelector('.popup__close-button');
-const popupFullPhotoPhoto = popupFullPhoto.querySelector('.popup__photo');
-const popupFullPhotoFigcaption = popupFullPhoto.querySelector('.popup__figcaption');
-
-const profileName = document.querySelector('.profile__name');
-const profileVocation = document.querySelector('.profile__vocation');
-
-const galleryList = document.querySelector('.gallery__list');
-const galleryItem = document.querySelector('#gallery-item').content; // template элемента галереи
-/* Обработчик изменения данных профиля */
-
-/* открытие popup */
-const showPopup = (popup) => {
-  switch (popup) {
-    case popupEditProfile:
-      formName.value = profileName.textContent;
-      formVocation.value = profileVocation.textContent;
-      break;
-    case popupAddPhoto:
-      formFigcaption.value = '';
-      formPhotoLink.value = '';
-      break;
-  }
-  let CloseButton = popup.querySelector('.popup__close-button');
-  CloseButton.addEventListener('click', () => {
-    closePopup(popup);
-  })
-  popup.classList.add('popup_opened');
-}
-
-/* закртыие popup */
-const closePopup = (popup) => {
-  popup.classList.remove('popup_opened');
-}
-
-function formPopupEditProfile(evt) {
-  evt.preventDefault(); // отменяет стандартную отправку формы.
-  profileName.textContent = formName.value.trim() ? formName.value : 'Без имени'; /* .trim() - удаляет пробелы с начала и конца строки, и повторяющиеся пробелы*/
-  profileVocation.textContent = formVocation.value.trim() ? formVocation.value : 'Без хобби';
-  addTitle(profileName);
-  addTitle(profileVocation);
-  closePopup(popupEditProfile);
-}
-
-function formPopupAddPhoto(evt) {
-  evt.preventDefault(); // отменяет стандартную отправку формы.
-  addPhoto (formFigcaption.value, formPhotoLink.value);
-  closePopup(popupAddPhoto);
-}
+'use strict' /* :D */
 
 /* Добавление title */
-function addTitle (titleTarget) {
+function addTitleForContent (titleTarget) {
   titleTarget.title = titleTarget.textContent;
 }
 
-/* Функция добавления фотографии на страницу */
+/* открытие/закрытие popup */
+const showPopup = (popup) => popup.classList.add('popup_opened');
+const closePopup = (popup) => popup.classList.remove('popup_opened');
+
+/* открытие popup EditProfile*/
+const showPopupEditProfile = () => {
+  formName.value = profileName.textContent;
+  formVocation.value = profileVocation.textContent;
+  showPopup(popupEditProfile);
+}
+
+/* открытие popup AddPhoto*/
+const showPopupAddPhoto = () => {
+  formFigcaption.value = '';
+  formPhotoLink.value = '';
+  showPopup(popupAddPhoto);
+}
+
+function cancelSubmitFormPopupEditProfile(evt) {
+  evt.preventDefault(); // отменяет стандартную отправку формы.
+  profileName.textContent = formName.value.trim() ? formName.value : 'Без имени'; /* .trim() - удаляет пробелы с начала и конца строки, и повторяющиеся пробелы*/
+  profileVocation.textContent = formVocation.value.trim() ? formVocation.value : 'Без хобби';
+  addTitleForContent(profileName);
+  addTitleForContent(profileVocation);
+  closePopup(popupEditProfile);
+}
+
+/* Функция добавления фотографии на страницу с открытием попапа FullPhoto */
 function addPhoto (figCaption, photoLink) {
   let galleryElement = galleryItem.querySelector('.gallery__element').cloneNode(true);
   let galleryPhoto = galleryElement.querySelector('.gallery__photo');
@@ -110,7 +42,7 @@ function addPhoto (figCaption, photoLink) {
   galleryPhoto.src = !(photoLink === '') ? photoLink : document.querySelector('.profile__avatar').src;
   galleryPhoto.alt = !(figCaption === '') ? figCaption : profileName.textContent;
   galleryFigCaption.textContent = !(figCaption === '') ? figCaption : profileName.textContent;
-  addTitle(galleryFigCaption);
+  addTitleForContent(galleryFigCaption);
   galleryLike.addEventListener('click', (evt) => {
     evt.target.classList.toggle('gallery__like_active')
   })
@@ -125,22 +57,33 @@ function addPhoto (figCaption, photoLink) {
   galleryList.prepend(galleryElement);
 }
 
+function cancelSubmitFormPopupAddPhoto(evt) {
+  evt.preventDefault(); // отменяет стандартную отправку формы.
+  addPhoto (formFigcaption.value, formPhotoLink.value);
+  closePopup(popupAddPhoto);
+}
+
+/* Закрытие PopupEditProfile */
+popupFullPhotoCloseButton.addEventListener('click', () => closePopup(popupFullPhoto));
+
 /* Добавление шести фотографий из массива */
-initialCards.reverse().forEach(item => {
+initialCards.forEach(item => {
   addPhoto(item.name, item.link);
 })
 
-/* Открытие PopupEditProfile */
-editProfileButton.addEventListener('click', () => showPopup(popupEditProfile));
+/* Открытие/закрытие PopupEditProfile */
+editProfileButton.addEventListener('click', () => showPopupEditProfile());
+popupEditProfileCloseButton.addEventListener('click', () => closePopup(popupEditProfile));
 
-/* Открытие popupAddPhoto */
-addPhotoButton.addEventListener('click', () => showPopup(popupAddPhoto));
+/* Открытие/закрытие popupAddPhoto */
+addPhotoButton.addEventListener('click', () => showPopupAddPhoto());
+popupAddPhotoCloseButton.addEventListener('click', () => closePopup(popupAddPhoto));
 
 /* Сохранение данных о пользователе*/
-popupEditProfileForm.addEventListener('submit', formPopupEditProfile);
+popupEditProfileForm.addEventListener('submit', cancelSubmitFormPopupEditProfile);
 
 /* Добавление фотографии в галерею*/
-popupAddPhotoForm.addEventListener('submit', formPopupAddPhoto);
+popupAddPhotoForm.addEventListener('submit', cancelSubmitFormPopupAddPhoto);
 
 
 
