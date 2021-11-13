df75d04c93d73b4cd9346bf5e67af21578a88b8f
KDA <iamkda@mail.ru>
2021-11-13 16:58:53 +0300

    change: let to const

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,60 @@
 'use strict' /* :D */
 
+/* Массив с начальными карточками */
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+/* Попап изменения данных о себе любимом */
+const editProfileButton = document.querySelector('.profile__edit-profile-button');
+const popupEditProfile = document.querySelector('.popup_edit-profile');
+const popupEditProfileCloseButton = popupEditProfile.querySelector('.popup__close-button');
+const formName = popupEditProfile.querySelector('.popup__text_parameter_name');
+const formVocation = popupEditProfile.querySelector('.popup__text_parameter_vocation');
+const popupEditProfileForm = popupEditProfile.querySelector('.popup__container');
+
+/* Попап добавления фотографии в галерею */
+const addPhotoButton = document.querySelector('.profile__edit-gallery-button');
+const popupAddPhoto = document.querySelector('.popup_add-photo');
+const popupAddPhotoCloseButton = popupAddPhoto.querySelector('.popup__close-button');
+const formFigcaption = popupAddPhoto.querySelector('.popup__text_parameter_figcaption');
+const formPhotoLink = popupAddPhoto.querySelector('.popup__text_parameter_photo-link');
+const popupAddPhotoForm = popupAddPhoto.querySelector('.popup__container');
+
+/* Попап открытия фотографий */
+const popupFullPhoto = document.querySelector('.popup_full-photo');
+const popupFullPhotoCloseButton = popupFullPhoto.querySelector('.popup__close-button');
+const popupFullPhotoPhoto = popupFullPhoto.querySelector('.popup__photo');
+const popupFullPhotoFigcaption = popupFullPhoto.querySelector('.popup__figcaption');
+
+const profileName = document.querySelector('.profile__name');
+const profileVocation = document.querySelector('.profile__vocation');
+
+const galleryList = document.querySelector('.gallery__list');
+const galleryItem = document.querySelector('#gallery-item').content; // template элемента галереи
+
 /* Добавление title */
 function addTitleForContent (titleTarget) {
   titleTarget.title = titleTarget.textContent;
@@ -34,11 +89,11 @@ function cancelSubmitFormPopupEditProfile(evt) {
 
 /* Функция добавления фотографии на страницу с открытием попапа FullPhoto */
 function addPhoto (figCaption, photoLink) {
-  let galleryElement = galleryItem.querySelector('.gallery__element').cloneNode(true);
-  let galleryPhoto = galleryElement.querySelector('.gallery__photo');
-  let galleryFigCaption = galleryElement.querySelector('.gallery__figcaption');
-  let galleryLike = galleryElement.querySelector('.gallery__like');
-  let galleryTrash = galleryElement.querySelector('.gallery__trash');
+  const galleryElement = galleryItem.querySelector('.gallery__element').cloneNode(true);
+  const galleryPhoto = galleryElement.querySelector('.gallery__photo');
+  const galleryFigCaption = galleryElement.querySelector('.gallery__figcaption');
+  const galleryLike = galleryElement.querySelector('.gallery__like');
+  const galleryTrash = galleryElement.querySelector('.gallery__trash');
   galleryPhoto.src = !(photoLink === '') ? photoLink : document.querySelector('.profile__avatar').src;
   galleryPhoto.alt = !(figCaption === '') ? figCaption : profileName.textContent;
   galleryFigCaption.textContent = !(figCaption === '') ? figCaption : profileName.textContent;
@@ -51,6 +106,7 @@ function addPhoto (figCaption, photoLink) {
   })
   galleryPhoto.addEventListener('click', () => {
     popupFullPhotoPhoto.src = galleryPhoto.src;
+    popupFullPhotoPhoto.alt = galleryFigCaption.textContent;
     popupFullPhotoFigcaption.textContent = galleryFigCaption.textContent;
     showPopup(popupFullPhoto);
   })
