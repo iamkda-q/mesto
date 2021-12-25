9e85f4f420c4cad7a80651d2d503ae7d467c4e75
KDA <iamkda@mail.ru>
2021-12-25 23:04:07 +0300

    add changes after revue2

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,15 +3,14 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._photo = this._popup.querySelector(".popup__photo");
+    this._photoFigcaption = this._popup.querySelector(".popup__figcaption");
   }
 
   open(photoLink, figCaption) {
-    this._photo = this._popup.querySelector(".popup__photo");
-    this._photoFigcaption = this._popup.querySelector(".popup__figcaption");
     this._photo.src = photoLink;
     this._photo.alt = figCaption;
     this._photoFigcaption.textContent = figCaption;
     super.open();
   }
-
 }
